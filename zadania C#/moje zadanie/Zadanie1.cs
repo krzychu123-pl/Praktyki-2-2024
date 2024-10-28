acd2474c6980eae88bd5db2a8ecd16cdb4c975f4
krzychu123-pl <krzysiulukosz@gmail.com>
2024-10-28 11:03:31 +0100

    Poprawienie zadania, aby po wprowadzeniu litery program włączał się ponownie

--- zadania C#/moje zadanie/Zadanie1.cs
+++ zadania C#/moje zadanie/Zadanie1.cs
@@ -3,27 +3,43 @@
 class Zadanie1
 {
     static void Main(string[] args)
-	{
-        Console.WriteLine("Wprowadź pierwszą liczbę:");
-        var zmienna1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Wprowadź drugą liczbę:");
-        var zmienna2 = Convert.ToDouble(Console.ReadLine());
-
-        var suma = zmienna1 + zmienna2;
-        var odejmowanie = zmienna1 - zmienna2;
-        var mnozenie = zmienna1 * zmienna2;
-        var dzielenie = (zmienna2 != 0 ) ? zmienna1 / zmienna2 : 0;
-
-        Console.WriteLine("\n\nWynik sumy: " + suma);
-        Console.WriteLine("Wynik odejmowania: " + odejmowanie);
-        Console.WriteLine("Wynik mnozenia: " + mnozenie);
-        if (zmienna2 != 0)
+    {
+        while (true)
         {
-            Console.WriteLine("Wynik dzielenia: " + dzielenie);
-        }
-        else { Console.WriteLine("Nie można dzielić przez 0"); }
+            try
+            {
+                Console.WriteLine("Wprowadź pierwszą liczbę:");
+                var zmienna1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("\n\nnaciśnij dowolny przycisk");
-        Console.ReadKey();
+                Console.WriteLine("Wprowadź drugą liczbę:");
+                var zmienna2 = Convert.ToDouble(Console.ReadLine());
+
+                var suma = zmienna1 + zmienna2;
+                var odejmowanie = zmienna1 - zmienna2;
+                var mnozenie = zmienna1 * zmienna2;
+                var dzielenie = (zmienna2 != 0) ? zmienna1 / zmienna2 : 0;
+
+                Console.WriteLine("\n\nWynik sumy: " + suma);
+                Console.WriteLine("Wynik odejmowania: " + odejmowanie);
+                Console.WriteLine("Wynik mnożenia: " + mnozenie);
+
+                if (zmienna2 != 0)
+                {
+                    Console.WriteLine("Wynik dzielenia: " + dzielenie);
+                }
+                else
+                {
+                    Console.WriteLine("Nie można dzielić przez 0");
+                }
+
+                Console.WriteLine("\n\nNaciśnij dowolny przycisk, aby zakończyć...");
+                Console.ReadKey();
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Wprowadzono niepoprawne dane! Proszę wpisać liczby.");
+            }
+        }
     }
 }
